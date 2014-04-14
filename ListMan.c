1ee4518aab84b9bc4731d62175e167e53d917b29
achasse <achasse@asu.edu>
2014-04-14 10:23:19 -0700

    Update ListMan.c

--- ListMan.c
+++ ListMan.c
@@ -1,6 +1,10 @@
 /***************************************************************************************************************
  * FILE: ListMan.c
  *
+ * Aaron Chasse
+ * CSE 220 - Lab 3 Extra Credit
+ * Spring 2014 
+ * 
  * DESCRIPTION
  * See comments in ListMan.h
  *
